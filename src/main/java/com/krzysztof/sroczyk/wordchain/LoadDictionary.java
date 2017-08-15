6919ec8e66a1976db3d99c03266a76f42b20d500
Krzysztof Sroczyk <krzysztof.sroczyk@ericsson.com>
2017-08-15 09:04:51 +0200

    correction in loading method

--- src/main/java/com/krzysztof/sroczyk/wordchain/LoadDictionary.java
+++ src/main/java/com/krzysztof/sroczyk/wordchain/LoadDictionary.java
@@ -1,24 +1,25 @@
 package com.krzysztof.sroczyk.wordchain;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class LoadDictionary {
 
-    List<String> loadWordList(final int wordLength) {
-        List<String> wordList;
-        String fileName = "dictionary.txt";
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(fileName).getFile());
+    public List<String> loadWordList(final int wordLength) {
+        List<String> wordList = new ArrayList<>();
+        String fileName = "/dictionary.txt";
 
-        try (Stream<String> stream = Files.lines(Paths.get(file.toURI()), Charset.forName("ISO-8859-1"))) {
-            wordList = stream.filter(readLine -> readLine.length() == wordLength).collect(Collectors.toList());
+        try {
+            InputStream in = getClass().getResourceAsStream(fileName);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String word;
+            while ((word = reader.readLine()) != null) {
+                if (word.length() == wordLength) {
+                    wordList.add(word);
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException("Exception while reading the file", e);
         }
